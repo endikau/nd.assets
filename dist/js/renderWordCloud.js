7f8489d7159eda940e4c751a6acbc02c03173f15
m-pilarski <89189235+m-pilarski@users.noreply.github.com>
2025-12-07 17:33:14 +0100

    update

--- dist/js/renderWordCloud.js
+++ dist/js/renderWordCloud.js
@@ -32,31 +32,66 @@ function renderWordCloud(words, containerId) {
   const width = container.offsetWidth;
   const height = container.offsetHeight;
 
-  const layout = d3.layout.cloud()
-    .size([width, height])
-    .words(words.map(d => Object.assign({}, d)))
-    .padding(5)
-    .font("Open Sans")
-    .fontSize(d => d.size)
-    .rotate(d => d.rotate)
-    .on("end", draw);
+  const svg = d3.select("#" + containerId).append("svg")
+    .attr("viewBox", `0 0 ${width} ${height}`)
+    .append("g")
+    .attr("transform", `translate(${width / 2},${height / 2})`);
 
-  layout.start();
+  d3.layout.cloud()
+    .size([width, height])
+    .words(words)
+    .padding(2)
+    .rotate(d => d.rotate)
+    .fontSize(d => d.size)
+    .font("Open Sans")
+    .spiral("rectangular")
+    .on("end", draw)
+    .start();
 
   function draw(words) {
-    d3.select("#" + containerId).append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-      .selectAll("text")
+    svg.selectAll("text")
       .data(words)
       .enter().append("text")
-      .style("font-size", d => d.size + "px")
       .style("font-family", "Open Sans")
       .style("fill", d => d.color)
+      .style("font-size", d => d.size + "px")
       .attr("text-anchor", "middle")
-      .attr("transform", d => "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")")
-      .text(d => d.term);
+      // .attr("transform", d => `translate(${d.x},${d.y}) rotate(${d.rotate})`)
+      .attr("transform", d => "translate(" + d.x + "," + d.y + ") rotate(" + d.rotate + ")")
+      .text(d => d.text);
   }
 }
+
+
+// function renderWordCloud(words, containerId) {
+//   const container = document.getElementById(containerId);
+//   if (!container) return;
+
+//   container.innerHTML = "";
+
+//   const width = container.offsetWidth;
+//   const height = container.offsetHeight;
+
+//   d3.layout.cloud()
+//     .size([width, height])
+//     .words(words.map(d => ({ ...d })))
+//     .padding(1)
+//     .font("Source Serif 4")
+//     .fontSize(d => d.size)
+//     .rotate(() => 0) // stick to horizontal for rectangular layout
+//     .spiral("rectangular") // <-- switch here
+//     .on("end", draw)
+//     .start();
+
+//   function draw(words) {
+//     svg.selectAll("text")
+//       .data(words)
+//       .enter().append("text")
+//       .style("font-family", "Source Serif 4")
+//       .style("fill", () => d3.schemeCategory10[Math.floor(Math.random() * 10)])
+//       .style("font-size", d => d.size + "px")
+//       .attr("text-anchor", "middle")
+//       .attr("transform", d => `translate(${d.x},${d.y}) rotate(${d.rotate})`)
+//       .text(d => d.text);
+//   }
+// }
